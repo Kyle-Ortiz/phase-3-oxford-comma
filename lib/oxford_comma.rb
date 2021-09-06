f739b108ec0c82c198b96fadc21748150f6ef626
Kyle Ortiz <kyleaortiz@gmail.com>
2021-09-06 11:52:00 -0500

    commit tree e253a5a2c0efdfe5633dc64524afcd781a6bc99f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+     if array.length == 1
+          array.join("")
+     else if array.length == 2 
+          array.join(" and ")
+     else
+          last = "and #{array[-1]}"
+          array.pop()
+          array.push(last)
+          array.join(", ")
+     end
+end
 end
